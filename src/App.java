2c7e66b9168b52d3696e63eb5046bd2849dbe5f3
Rafael <rafael2004benicio@gmail.com>
2024-03-20 12:13:41 -0300

    Meu terceiro commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Olá mundo");
+        System.out.println("Olá mundo 23//03/2024");
     }
 }
